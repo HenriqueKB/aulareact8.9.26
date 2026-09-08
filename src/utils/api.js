4f48afa854c196c4eb8420cdfe6716095480408b
HenriqueKB <henriqueruizmachado7@gmail.com>
2026-09-08 20:35:27 -0300

    a

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: '',
+    baseURL: 'https://rickandmortyapi.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
